727247086a359917568c3ce7235c78ad96658ea6
Mike Kreiser <mkreise2@illinois.edu>
2017-03-26 18:57:17 -0500

    Change 100m HH conversion. Add new outdoor results

--- submodules/convertEvents.py
+++ submodules/convertEvents.py
@@ -26,7 +26,7 @@ def convertEvents(indoorResultsObject):
 
   # Convert indoor events to outdoor equivalents
   indoorResultsObject['100m'] = convertSingleEvent(indoorResultsObject['60m'], lambda x: x*2 - 3.3)
-  indoorResultsObject['100m HH'] = convertSingleEvent(indoorResultsObject['60m HH'], lambda x: x*2 - 3.1)
+  indoorResultsObject['100m HH'] = convertSingleEvent(indoorResultsObject['60m HH'], lambda x: x*2 - 2)
   indoorResultsObject['1500m'] = convertSingleEvent(indoorResultsObject['1 Mile'], lambda x: x * 0.9259)
   indoorResultsObject['5000m'] = convertSingleEvent(indoorResultsObject['3000m'], lambda x: x * ((5/3)**1.06))
 
